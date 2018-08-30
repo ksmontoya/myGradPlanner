df4c4dd6fdba06421376df625968151919b799e7
Arnoldo-Linux <amontoya@iastate.edu>
2018-08-30 10:17:03 -0500

    Added a very basic Spring Server that will return a String if http request is made.[Experiment]

---
+++ experiments/Arnoldo_Experiments/Back_End/experiment/src/main/java/com/example/demo/ExperimentApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class ExperimentApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello Karla!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ExperimentApplication.class, args);
+	}
+}
